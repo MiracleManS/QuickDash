7a11c88b98ed5abc11a81f72cc2ad41230a22f15
Dustin Sier <dustin.sier@gmail.com>
2013-12-06 13:03:50 -0500

    Added cross orgin header to allow pulling from any domain.

--- api.php
+++ api.php
@@ -6,6 +6,7 @@ include ('lib/functions.php');
 if (!empty($_GET) && !empty($_GET['id'])) {
   
   header('Content-Type: application/json');
+  header("access-control-allow-origin: *");
   echo return_item_json($_GET['id']);
   
 } else {
